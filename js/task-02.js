cb16bfb92172939255a7d997034ea43dd2c0c01c
Dasha Yarkovska <yarkovska11@gmail.com>
2022-12-28 08:06:10 +0200

    task 3 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,25 @@
+// c: initial arr 👇
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+//task Напиши скрипт, який для кожного елемента масиву ingredients:
+//f.e.Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+//f.e Додасть назву інгредієнта як його текстовий вміст.
+//f.e Додасть елементу клас item.
+//f.e Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+const createLiEls = (ingredientsArr) => {
+	const elsArr = ingredientsArr.map((ingredient) => {
+		const itemEl = document.createElement("li");
+		itemEl.classList.add("item");
+		itemEl.textContent = ingredient;
+
+		return itemEl;
+	});
+
+	const listEl = document.querySelector("#ingredients");
+	listEl.append(...elsArr);
+
+	// return listEl; //? uncomment to see list in console
+};
+
+console.log(createLiEls(ingredients));
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,34 @@
+//c: initial arr 👇
 const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
+	{
+		url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		alt: "White and Black Long Fur Cat",
+	},
+	{
+		url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+	},
+	{
+		url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		alt: "Group of Horses Running",
+	},
 ];
+
+// task Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+//f.e Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+//f.e Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+//f.e Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
+const makeGalleryMarkup = images
+	.map(
+		({ url, alt }) =>
+			`<li>
+			<img src="${url} alt="${alt} height="500">
+			</li>`
+	)
+	.join("");
+
+const listEl = document.querySelector(".gallery");
+listEl.style.display = "flex";
+listEl.insertAdjacentHTML("beforeend", makeGalleryMarkup);
