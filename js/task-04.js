b15ad5ba85bdf45efcde4c419ae9ce762390c437
Dasha Yarkovska <yarkovska11@gmail.com>
2022-12-28 15:08:10 +0200

    task 4 done

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,24 @@
-//відслітковувати натискання на пробіл перезавантажувати вікну (у задачі про рандомні кольори)
+//task Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
+//f.e Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+//f.e Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+
+const counterDecrementEl = document.querySelector('button[data-action="decrement"]');
+const counterIncrementEl = document.querySelector('button[data-action="increment"]');
+const counterNumberEl = document.querySelector("#value");
+let counterValue = 0;
+counterNumberEl.textContent = counterValue;
+
+const increaseFunc = () => {
+	counterNumberEl.textContent = counterValue + 1;
+	return (counterValue += 1);
+};
+const decreaseFunc = () => {
+	counterNumberEl.textContent = counterValue - 1;
+	return (counterValue -= 1);
+};
+
+increaseFunc();
+decreaseFunc();
+
+counterDecrementEl.addEventListener("click", decreaseFunc);
+counterIncrementEl.addEventListener("click", increaseFunc);
